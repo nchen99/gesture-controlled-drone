2ef88db552ec10cf093b72f0d4113f4a017f54dd
Fengpeng Huang <patrickwong.ubcengineering@gmail.com>
2022-01-20 15:02:51 -0800

    step 1

--- main.py
+++ main.py
@@ -1,192 +1,192 @@
-from djitellopy import Tello
-import cv2
-import pygame
-import numpy as np
-import time
-# mediapipe
-import mediapipe as mp
-mp_face_detection = mp.solutions.face_detection
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
+# from djitellopy import Tello
+# import cv2
+# import pygame
+# import numpy as np
+# import time
+# # mediapipe
+# import mediapipe as mp
+# mp_face_detection = mp.solutions.face_detection
+# mp_drawing = mp.solutions.drawing_utils
+# mp_drawing_styles = mp.solutions.drawing_styles
+# mp_hands = mp.solutions.hands
 
-# Speed of the drone
-S = 60
-# Frames per second of the pygame window display
-# A low number also results in input lag, as input information is processed once per frame.
-FPS = 30
-
-
-class FrontEnd(object):
-    """ Maintains the Tello display and moves it through the keyboard keys.
-        Press escape key to quit.
-        The controls are:
-            - T: Takeoff
-            - L: Land
-            - Arrow keys: Forward, backward, left and right.
-            - A and D: Counter clockwise and clockwise rotations (yaw)
-            - W and S: Up and down
-    """
-
-    def __init__(self):
-        # Init pygame
-        pygame.init()
-
-        # Creat pygame window
-        pygame.display.set_caption("Tello video stream")
-        self.screen = pygame.display.set_mode([960, 720])
-
-        # Init Tello object that interacts with the Tello drone
-        self.tello = Tello()
-
-        # Drone velocities between -100~100
-        self.for_back_velocity = 0
-        self.left_right_velocity = 0
-        self.up_down_velocity = 0
-        self.yaw_velocity = 0
-        self.speed = 10
-        self.take_picture = False
-
-        self.send_rc_control = False
-
-        # create update timer
-        pygame.time.set_timer(pygame.USEREVENT + 1, 1000 // FPS)
-
-    def run(self):
-
-        self.tello.connect()
-        self.tello.set_speed(self.speed)
-
-        # In case streaming is on. This happens when we quit this program without the escape key.
-        self.tello.streamoff()
-        self.tello.streamon()
-
-        frame_read = self.tello.get_frame_read()
-
-        should_stop = False
-
-        with mp_face_detection.FaceDetection(
-                model_selection=1, min_detection_confidence=0.5) as face_detection:
-            with mp_hands.Hands(
-                    static_image_mode=True,
-                    max_num_hands=2,
-                    min_detection_confidence=0.5) as hands:
-                while not should_stop:
-
-                    for event in pygame.event.get():
-                        if event.type == pygame.USEREVENT + 1:
-                            self.update()
-                        elif event.type == pygame.QUIT:
-                            should_stop = True
-                        elif event.type == pygame.KEYDOWN:
-                            if event.key == pygame.K_ESCAPE:
-                                should_stop = True
-                            else:
-                                self.keydown(event.key)
-                        elif event.type == pygame.KEYUP:
-                            self.keyup(event.key)
-
-                    if frame_read.stopped:
-                        break
-
-                    self.screen.fill([0, 0, 0])
-
-                    frame = frame_read.frame
-
-                    if self.take_picture:
-                        self.take_picture = False
-                        cv2.imwrite(f"./out/{time.time_ns()}.png", frame)
-
-                    # face detection
-                    fd_results = face_detection.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-                    if fd_results.detections:
-                        for detection in fd_results.detections:
-                            mp_drawing.draw_detection(frame, detection)
-                    # hand detection
-                    hd_results = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-                    if hd_results.multi_hand_landmarks:
-                        for hand_landmarks in hd_results.multi_hand_landmarks:
-                            mp_drawing.draw_landmarks(
-                                frame,
-                                hand_landmarks,
-                                mp_hands.HAND_CONNECTIONS,
-                                mp_drawing_styles.get_default_hand_landmarks_style(),
-                                mp_drawing_styles.get_default_hand_connections_style())
-
-                    # battery n.
-                    text = "Battery: {}%".format(self.tello.get_battery())
-                    cv2.putText(frame, text, (5, 720 - 5),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    frame = np.rot90(frame)
-                    frame = np.flipud(frame)
-
-                    frame = pygame.surfarray.make_surface(frame)
-                    self.screen.blit(frame, (0, 0))
-                    pygame.display.update()
-
-                    time.sleep(1 / FPS)
-
-        # Call it always before finishing. To deallocate resources.
-        self.tello.end()
-
-    def keydown(self, key):
-        """ Update velocities based on key pressed
-        Arguments:
-            key: pygame key
-        """
-        if key == pygame.K_UP:  # set forward velocity
-            self.for_back_velocity = S
-        elif key == pygame.K_DOWN:  # set backward velocity
-            self.for_back_velocity = -S
-        elif key == pygame.K_LEFT:  # set left velocity
-            self.left_right_velocity = -S
-        elif key == pygame.K_RIGHT:  # set right velocity
-            self.left_right_velocity = S
-        elif key == pygame.K_w:  # set up velocity
-            self.up_down_velocity = S
-        elif key == pygame.K_s:  # set down velocity
-            self.up_down_velocity = -S
-        elif key == pygame.K_a:  # set yaw counter clockwise velocity
-            self.yaw_velocity = -S
-        elif key == pygame.K_d:  # set yaw clockwise velocity
-            self.yaw_velocity = S
-        elif key == pygame.K_SPACE:  # take a picture
-            self.take_picture = True
-
-    def keyup(self, key):
-        """ Update velocities based on key released
-        Arguments:
-            key: pygame key
-        """
-        if key == pygame.K_UP or key == pygame.K_DOWN:  # set zero forward/backward velocity
-            self.for_back_velocity = 0
-        elif key == pygame.K_LEFT or key == pygame.K_RIGHT:  # set zero left/right velocity
-            self.left_right_velocity = 0
-        elif key == pygame.K_w or key == pygame.K_s:  # set zero up/down velocity
-            self.up_down_velocity = 0
-        elif key == pygame.K_a or key == pygame.K_d:  # set zero yaw velocity
-            self.yaw_velocity = 0
-        elif key == pygame.K_t:  # takeoff
-            self.tello.takeoff()
-            self.send_rc_control = True
-        elif key == pygame.K_l:  # land
-            not self.tello.land()
-            self.send_rc_control = False
-
-    def update(self):
-        if self.send_rc_control:
-            self.tello.send_rc_control(self.left_right_velocity, self.for_back_velocity,
-                self.up_down_velocity, self.yaw_velocity)
+# # Speed of the drone
+# S = 60
+# # Frames per second of the pygame window display
+# # A low number also results in input lag, as input information is processed once per frame.
+# FPS = 30
 
 
-def main():
-    frontend = FrontEnd()
+# class FrontEnd(object):
+#     """ Maintains the Tello display and moves it through the keyboard keys.
+#         Press escape key to quit.
+#         The controls are:
+#             - T: Takeoff
+#             - L: Land
+#             - Arrow keys: Forward, backward, left and right.
+#             - A and D: Counter clockwise and clockwise rotations (yaw)
+#             - W and S: Up and down
+#     """
 
-    # run frontend
+#     def __init__(self):
+#         # Init pygame
+#         pygame.init()
 
-    frontend.run()
+#         # Creat pygame window
+#         pygame.display.set_caption("Tello video stream")
+#         self.screen = pygame.display.set_mode([960, 720])
+
+#         # Init Tello object that interacts with the Tello drone
+#         self.tello = Tello()
+
+#         # Drone velocities between -100~100
+#         self.for_back_velocity = 0
+#         self.left_right_velocity = 0
+#         self.up_down_velocity = 0
+#         self.yaw_velocity = 0
+#         self.speed = 10
+#         self.take_picture = False
+
+#         self.send_rc_control = False
+
+#         # create update timer
+#         pygame.time.set_timer(pygame.USEREVENT + 1, 1000 // FPS)
+
+#     def run(self):
+
+#         self.tello.connect()
+#         self.tello.set_speed(self.speed)
+
+#         # In case streaming is on. This happens when we quit this program without the escape key.
+#         self.tello.streamoff()
+#         self.tello.streamon()
+
+#         frame_read = self.tello.get_frame_read()
+
+#         should_stop = False
+
+#         with mp_face_detection.FaceDetection(
+#                 model_selection=1, min_detection_confidence=0.5) as face_detection:
+#             with mp_hands.Hands(
+#                     static_image_mode=True,
+#                     max_num_hands=2,
+#                     min_detection_confidence=0.5) as hands:
+#                 while not should_stop:
+
+#                     for event in pygame.event.get():
+#                         if event.type == pygame.USEREVENT + 1:
+#                             self.update()
+#                         elif event.type == pygame.QUIT:
+#                             should_stop = True
+#                         elif event.type == pygame.KEYDOWN:
+#                             if event.key == pygame.K_ESCAPE:
+#                                 should_stop = True
+#                             else:
+#                                 self.keydown(event.key)
+#                         elif event.type == pygame.KEYUP:
+#                             self.keyup(event.key)
+
+#                     if frame_read.stopped:
+#                         break
+
+#                     self.screen.fill([0, 0, 0])
+
+#                     frame = frame_read.frame
+
+#                     if self.take_picture:
+#                         self.take_picture = False
+#                         cv2.imwrite(f"./out/{time.time_ns()}.png", frame)
+
+#                     # face detection
+#                     fd_results = face_detection.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+#                     if fd_results.detections:
+#                         for detection in fd_results.detections:
+#                             mp_drawing.draw_detection(frame, detection)
+#                     # hand detection
+#                     hd_results = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+#                     if hd_results.multi_hand_landmarks:
+#                         for hand_landmarks in hd_results.multi_hand_landmarks:
+#                             mp_drawing.draw_landmarks(
+#                                 frame,
+#                                 hand_landmarks,
+#                                 mp_hands.HAND_CONNECTIONS,
+#                                 mp_drawing_styles.get_default_hand_landmarks_style(),
+#                                 mp_drawing_styles.get_default_hand_connections_style())
+
+#                     # battery n.
+#                     text = "Battery: {}%".format(self.tello.get_battery())
+#                     cv2.putText(frame, text, (5, 720 - 5),
+#                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+#                     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#                     frame = np.rot90(frame)
+#                     frame = np.flipud(frame)
+
+#                     frame = pygame.surfarray.make_surface(frame)
+#                     self.screen.blit(frame, (0, 0))
+#                     pygame.display.update()
+
+#                     time.sleep(1 / FPS)
+
+#         # Call it always before finishing. To deallocate resources.
+#         self.tello.end()
+
+#     def keydown(self, key):
+#         """ Update velocities based on key pressed
+#         Arguments:
+#             key: pygame key
+#         """
+#         if key == pygame.K_UP:  # set forward velocity
+#             self.for_back_velocity = S
+#         elif key == pygame.K_DOWN:  # set backward velocity
+#             self.for_back_velocity = -S
+#         elif key == pygame.K_LEFT:  # set left velocity
+#             self.left_right_velocity = -S
+#         elif key == pygame.K_RIGHT:  # set right velocity
+#             self.left_right_velocity = S
+#         elif key == pygame.K_w:  # set up velocity
+#             self.up_down_velocity = S
+#         elif key == pygame.K_s:  # set down velocity
+#             self.up_down_velocity = -S
+#         elif key == pygame.K_a:  # set yaw counter clockwise velocity
+#             self.yaw_velocity = -S
+#         elif key == pygame.K_d:  # set yaw clockwise velocity
+#             self.yaw_velocity = S
+#         elif key == pygame.K_SPACE:  # take a picture
+#             self.take_picture = True
+
+#     def keyup(self, key):
+#         """ Update velocities based on key released
+#         Arguments:
+#             key: pygame key
+#         """
+#         if key == pygame.K_UP or key == pygame.K_DOWN:  # set zero forward/backward velocity
+#             self.for_back_velocity = 0
+#         elif key == pygame.K_LEFT or key == pygame.K_RIGHT:  # set zero left/right velocity
+#             self.left_right_velocity = 0
+#         elif key == pygame.K_w or key == pygame.K_s:  # set zero up/down velocity
+#             self.up_down_velocity = 0
+#         elif key == pygame.K_a or key == pygame.K_d:  # set zero yaw velocity
+#             self.yaw_velocity = 0
+#         elif key == pygame.K_t:  # takeoff
+#             self.tello.takeoff()
+#             self.send_rc_control = True
+#         elif key == pygame.K_l:  # land
+#             not self.tello.land()
+#             self.send_rc_control = False
+
+#     def update(self):
+#         if self.send_rc_control:
+#             self.tello.send_rc_control(self.left_right_velocity, self.for_back_velocity,
+#                 self.up_down_velocity, self.yaw_velocity)
 
 
-if __name__ == '__main__':
-    main()
+# def main():
+#     frontend = FrontEnd()
+
+#     # run frontend
+
+#     frontend.run()
+
+
+# if __name__ == '__main__':
+#     main()
