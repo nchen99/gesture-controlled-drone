6589ade030ca4bf83f6dc1db22240e35b2a2b71a
Fengpeng Huang <patrickwong.ubcengineering@gmail.com>
2022-01-20 16:20:39 -0800

    add gesture code comment.

--- src/utils/uav_utils.py
+++ src/utils/uav_utils.py
@@ -18,6 +18,30 @@ def connect_uav():
 
 def manual_control(uav, q):
     ## Add State Machine here
+    # Gesture code to be used:
+    # gesture_categories = [
+    #     '0',
+    #     '1',
+    #     '2',
+    #     '3',
+    #     '4',
+    #     '5',
+    #     '6',
+    #     '7',
+    #     '8',
+    #     '9',
+    #     'left',
+    #     'ok',
+    #     'right',
+    #     'rock',
+    #     'finger heart',
+    #     'praise',
+    #     'prayer',
+    #     'stop',
+    #     'Give the middle finger',
+    #     'bow',
+    #     'No gesture'
+    # ]
     while True:
         val = q.get()
         print(val)
