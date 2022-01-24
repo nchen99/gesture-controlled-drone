496b99e591474bf46725cab75cd59467b91f08ba
Fengpeng Huang <patrickwong.ubcengineering@gmail.com>
2022-01-24 13:46:20 -0800

    push to test 2

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import time
 import _thread
 from utils.uav_utils import connect_uav 
 from utils.RunModels import RunModel
-from pid_controllers import run_track
+from pid_controllers.run_track import init
 from utils.shared_variable import Shared
 
 
@@ -15,4 +15,4 @@ if __name__ == '__main__':
     shouldFollowMe = Shared(False)
 
     _thread.start_new_thread(RunModel, (uav, 0, shouldFollowMe)) #span hand gesture recognition
-    _thread.start_new_thread(run_track.init, (uav, shouldFollowMe)) #span follow me 
+    _thread.start_new_thread(init, (uav, shouldFollowMe)) #span follow me 
