ec5c724042d4eefa9b74ca183e59d68ad3167a80
Fengpeng Huang <patrickwong.ubcengineering@gmail.com>
2022-02-05 15:35:34 -0800

    delete presenter server

--- src/pid_controllers/run_track.py
+++ src/pid_controllers/run_track.py
@@ -17,18 +17,18 @@ import argparse
 sys.path.append("..")
 sys.path.append("../lib")
 
-from utils.params import params
-from atlas_utils.presenteragent import presenter_channel
-from atlas_utils.acl_image import AclImage
+# from utils.params import params
+# from atlas_utils.presenteragent import presenter_channel
+# from atlas_utils.acl_image import AclImage
 
 
 def init_presenter_server():
     SRC_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     PRESENTER_SERVER_CONF = os.path.join(SRC_PATH, "uav_presenter_server.conf")
-    chan = presenter_channel.open_channel(PRESENTER_SERVER_CONF)
-    if chan is None:
-        raise Exception("Open presenter channel failed")
-    return chan
+    # chan = presenter_channel.open_channel(PRESENTER_SERVER_CONF)
+    # if chan is None:
+    #     raise Exception("Open presenter channel failed")
+    # return chan
 
 def gen_save_name(tracker):
     now = datetime.now()
@@ -80,16 +80,16 @@ def parser():
     args = parser.parse_args()
     return args
 
-def send_to_presenter_server(chan, frame_org, result_img):
-    _, jpeg_image = cv2.imencode('.jpg', result_img)
-    jpeg_image = AclImage(jpeg_image, frame_org.shape[0], frame_org.shape[1], jpeg_image.size)
-    chan.send_detection_data(frame_org.shape[0], frame_org.shape[1], jpeg_image, [])
+# def send_to_presenter_server(chan, frame_org, result_img):
+#     _, jpeg_image = cv2.imencode('.jpg', result_img)
+#     jpeg_image = AclImage(jpeg_image, frame_org.shape[0], frame_org.shape[1], jpeg_image.size)
+#     chan.send_detection_data(frame_org.shape[0], frame_org.shape[1], jpeg_image, [])
 
 if __name__ == "__main__":
     args = parser()
 
-    if args.use_ps:
-        chan = init_presenter_server()
+    # if args.use_ps:
+    #     chan = init_presenter_server()
 
     x_err, y_err = 0, 0
     tookoff, flight_end = False, False
@@ -112,8 +112,8 @@ if __name__ == "__main__":
             frame_org = tracker.fetch_frame()
             x_err, y_err, result_img = tracker.run_state_machine(frame_org, x_err, y_err)
 
-            if args.use_ps:
-                send_to_presenter_server(chan, frame_org, result_img)
+            # if args.use_ps:
+            #     send_to_presenter_server(chan, frame_org, result_img)
 
         except (KeyboardInterrupt, Exception) as e:
             tracker.uav.land()
