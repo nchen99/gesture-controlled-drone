5ba9bda357f79672f4bd6aca99435dda1c37c8d2
Fengpeng Huang <patrickwong.ubcengineering@gmail.com>
2022-02-06 21:54:38 -0800

    push openpose_tf

--- src/openpose_tf.py
+++ src/openpose_tf.py
@@ -12,6 +12,7 @@ limitations under the License.
 """
 
 import os
+from turtle import right
 import cv2
 import numpy as np
 import argparse
@@ -583,6 +584,18 @@ def get_bounding_box(img):
 
     return results
 
+def check_pose(pose, human):
+    global threshold
+
+    bp = human.body_parts
+
+    for entry in pose:
+        if set(entry["req"]) <= set(bp):
+            if entry["check"](bp):
+                return True
+
+    return False
+
 
 def calculate_bounding_box(human, h, w):
     # logic: check for identifiable points
@@ -620,6 +633,8 @@ def calculate_bounding_box(human, h, w):
             "nose": nose,
             "neck": neck,
             "dist": int(math.sqrt((nose[0] - neck[0]) ** 2 + (nose[1] - neck[1]) ** 2)),
+            "right_arm_up": check_pose(check_pose[Pose.RIGHT_ARM_UP], human),
+            "left_arm_up": check_pose(check_pose[Pose.LEFT_ARM_UP], human),
         }
 
     return None
